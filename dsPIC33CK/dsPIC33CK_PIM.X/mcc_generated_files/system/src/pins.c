b4d22bafa1a87684edce7bbb2f0d2dbdf385827e
John Haroian <john.haroian@microchip.com>
2025-03-27 14:56:06 -0700

    Added UART1 and switched to external oscillator

--- dsPIC33CK/dsPIC33CK_PIM.X/mcc_generated_files/system/src/pins.c
+++ dsPIC33CK/dsPIC33CK_PIM.X/mcc_generated_files/system/src/pins.c
@@ -51,7 +51,7 @@ void PINS_Initialize(void)
     LATA = 0x0000;
     LATB = 0x0000;
     LATC = 0x0000;
-    LATD = 0x0000;
+    LATD = 0x0008;
 
     /****************************************************************************
      * Setting the GPIO Direction SFR(s)
@@ -59,7 +59,7 @@ void PINS_Initialize(void)
     TRISA = 0x001F;
     TRISB = 0xFFFF;
     TRISC = 0xFFFF;
-    TRISD = 0xFFFF;
+    TRISD = 0xFFF7;
 
 
     /****************************************************************************
@@ -88,9 +88,20 @@ void PINS_Initialize(void)
      * Setting the Analog/Digital Configuration SFR(s)
      ***************************************************************************/
     ANSELA = 0x001F;
-    ANSELB = 0x0387;
+    ANSELB = 0x0386;
     ANSELC = 0x00CF;
     ANSELD = 0x2C00;
+
+    /****************************************************************************
+     * Set the PPS
+     ***************************************************************************/
+     __builtin_write_RPCON(0x0000); // unlock PPS
+
+        RPINR18bits.U1RXR = 0x0044; //RD4->UART1:U1RX;
+        RPOR17bits.RP67R = 0x0001;  //RD3->UART1:U1TX;
+
+     __builtin_write_RPCON(0x0800); // lock PPS
+
 
 }
 
